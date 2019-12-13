f7576d2b325a6a0cfb049ac4291e93eb2a0428ba
Shuhe-Ma <55103783+Shuhe-Ma@users.noreply.github.com>
2019-12-12 22:06:42 -0500

    Add files via upload

---
+++ nycrime/doc/my-vignette.R
@@ -0,0 +1,20 @@
+## ----echo=FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+library(knitr)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(purrr)
+library(plyr)
+library(stringr)
+library(httr)
+library(nycrime)
+
+## ----setup---------------------------------------------------------------
+knitr::opts_chunk$set(include = FALSE)
+library(nycrime)
+
